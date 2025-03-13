ed15940553778306c93d7045100117c0eeedf650
ericliu22 <ericliu108@gmail.com>
2025-03-13 01:43:51 -0400

    Fixed eww_toggle

--- eww/scripts/eww_toggle.c
+++ eww/scripts/eww_toggle.c
@@ -1,24 +1,68 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <wayland-client.h>
+#include <wayland-client-protocol.h>
 
-void toggle(char* window) {
+static void registry_handler(void *data, struct wl_registry *registry,
+                             uint32_t name, const char *interface, uint32_t version) {
+    if (strcmp(interface, "wl_output") == 0) {
+        int *monitor_count = (int *)data;
+        (*monitor_count)++;
+        // Optionally, you can bind the output here:
+        // struct wl_output *output = wl_registry_bind(registry, name, &wl_output_interface, version);
+    }
 }
+
+static void registry_remover(void *data, struct wl_registry *registry, uint32_t name) {
+    // This function is called when a global is removed. In many cases you can leave it empty.
+}
+
 
 int main(int argc, char** argv) {
 
-	const char* window = argv[1];
+	struct wl_display *display = wl_display_connect(NULL);
+	if (!display) {
+	fprintf(stderr, "Failed to connect to Wayland display\n");
+	return EXIT_FAILURE;
+	}
+
+	struct wl_registry *registry = wl_display_get_registry(display);
+	int monitor_count = 0;
+
+	static const struct wl_registry_listener registry_listener = {
+		.global = registry_handler,
+		.global_remove = registry_remover,
+	};
+
+	wl_registry_add_listener(registry, &registry_listener, &monitor_count);
+
+	// Perform a roundtrip to process the registry events
+	wl_display_roundtrip(display);
+
+	// Clean up
+	wl_registry_destroy(registry);
+	wl_display_disconnect(display);
+
+	const char* input = argv[1];
 	FILE* fp;
+	char window[128];
 	char buffer[128];
-	char second_buffer[64];
+	char second_buffer[128];
 	char command[128];
 	int isOpen = 0;
 
 	if (argc != 2) {
-		fprintf(stderr, "Usage:\n	ewwtoggle <window>\n");
+		fprintf(stderr, "Usage:\n	eww_toggle <window>\n");
 		return EXIT_FAILURE;
 	}
 
+	if (monitor_count == 1) {
+		sprintf(window, "%salt", input);
+	} else {
+		sprintf(window, "%s", input);
+	}
 
 	fp = popen("eww active-windows | awk '{print $2}'", "r");
 	while (fgets(buffer, sizeof(buffer), fp) != NULL) {
